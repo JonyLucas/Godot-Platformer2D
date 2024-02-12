68e265204687a4a5075cfdafa308d1c1163825be
João Lucas <jony.lucas.br13@gmail.com>
2024-02-12 18:51:40 -0300

    Fix issues

--- scripts/Coin.gd
+++ scripts/Coin.gd
@@ -3,9 +3,10 @@ extends Node2D
 func _on_area_entered(body):
 	if body.get_parent().has_method("collect_coin"): 
 		body.get_parent().collect_coin()
-		$Area2D/CollisionShape2D.disabled = false
+		call_deferred("disable_collision")
 		$AnimationPlayer.play("pickup")
 
-func _on_collect_animation_finished(anim_name:StringName):
-	if anim_name == "pickup":
-		queue_free()
+
+func disable_collision():
+	$Area2D/CollisionShape2D.disabled = true
+
