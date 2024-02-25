858925b2216c2bad64b82054f1000b9215d6d89e
João Lucas <jony.lucas.br13@gmail.com>
2024-02-24 23:05:53 -0300

    Coin UI

---
+++ scripts/UICanvas.gd
@@ -0,0 +1,14 @@
+extends CanvasLayer
+
+var coin_label
+
+func _ready():
+	var level_manager = get_tree().get_first_node_in_group("level_manager")
+	coin_label = $MarginContainer/HBoxContainer/CoinLabel
+
+	if level_manager:
+		level_manager.connect("coin_collected_signal", update_coin_score, CONNECT_DEFERRED)
+		update_coin_score(0, level_manager.total_coins)
+
+func update_coin_score(coins_collected, total_coins):
+	coin_label.text = str(coins_collected) + " / " + str(total_coins)
