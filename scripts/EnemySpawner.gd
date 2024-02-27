2619903de7a99b2ca0c3f4d1c52f68d0e537678a
João Lucas <jony.lucas.br13@gmail.com>
2024-02-27 20:27:50 -0300

    Enemy Spawner

---
+++ scripts/EnemySpawner.gd
@@ -0,0 +1,32 @@
+extends Marker2D
+
+@export var enemy_scene: PackedScene
+@export var enemy_count: int
+@export var enemy_target_offset: float
+
+var current_enemy: Node2D = null
+var spawn_on_next_timeout: bool = false
+
+func _ready():
+	call_deferred("spawn_enemy")
+
+
+func spawn_enemy():
+	if enemy_scene != null:
+		current_enemy = enemy_scene.instantiate()
+		current_enemy.global_position = global_position
+		get_parent().add_child(current_enemy)
+		current_enemy.move_offset = enemy_target_offset
+		enemy_count -= 1
+
+
+func _on_spawn_timeout():
+	if is_instance_valid(current_enemy) or enemy_count <= 0:
+		return
+	
+	if spawn_on_next_timeout:
+		spawn_on_next_timeout = false
+		return
+
+	spawn_enemy()
+	spawn_on_next_timeout = false
