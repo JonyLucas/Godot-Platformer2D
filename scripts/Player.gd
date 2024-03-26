6da436e573ece2996b273bcbf861605025c932a0
João Lucas <jony.lucas.br13@gmail.com>
2024-03-26 18:32:16 -0300

    Fix Death Animation

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -109,10 +109,11 @@ func die(is_enemy_hazard: bool):
 
 	$"/root/Helpers".apply_camera_shake(0, 1)	
 	var death_scene = player_death_scene.instantiate()
-	death_scene.global_position = $AnimatedSprite2D.global_position
 	get_parent().add_child(death_scene)
+	death_scene.global_position = global_position
 	$AnimatedSprite2D.visible = false
-	# death_scene.velocity = velocity
+	death_scene.velocity = velocity
+	print(death_scene.global_position)
 	emit_signal("player_death_signal")
 
 
