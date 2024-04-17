52ff94a36c66e9bac82905308835471d96e38adc
João Lucas <jony.lucas.br13@gmail.com>
2024-04-16 23:49:31 -0300

    Enemy Death Animation Improvements

--- scripts/Enemy.gd
+++ scripts/Enemy.gd
@@ -3,6 +3,8 @@ extends CharacterBody2D
 @export var max_speed = 200
 @export var gravity = 500
 @export var move_offset = 5
+
+var enemy_death_scene = preload("res://Scenes/enemy_death.tscn")
 var direction = Vector2.RIGHT
 var target_x_position = 0
 
@@ -25,4 +27,8 @@ func _process(delta):
 
 func die():
 	$"/root/Helpers".apply_camera_shake(0, 1)
+	var death_scene = enemy_death_scene.instantiate()
+	death_scene.position = position
+	death_scene.scale = Vector2(1, 1) if direction.x > 0 else Vector2(-1, 1)
+	get_parent().add_child(death_scene)
 	queue_free()
