c8a1b1910ee4eef806bd8b7104dfe90cd13d3943
João Lucas <jony.lucas.br13@gmail.com>
2024-05-20 13:11:52 -0300

    Switch Panels Animation

--- scripts/OptionsMenu.gd
+++ scripts/OptionsMenu.gd
@@ -4,6 +4,10 @@ var is_full_screen = false
 
 @onready var window_mode_button = get_node("MarginContainer/PanelContainer/MarginContainer/VBoxContainer/HBoxContainer/WindowModeButton")
 
+func on_enable():
+	$AnimationPlayer.play("on_open")
+	pass
+
 func _on_window_mode_pressed():
 	is_full_screen = !is_full_screen
 	DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN if is_full_screen else DisplayServer.WINDOW_MODE_WINDOWED)
@@ -11,5 +15,6 @@ func _on_window_mode_pressed():
 
 
 func _on_back_button_pressed():
+	$AnimationPlayer.play("on_close")
 	get_parent().get_node("MainContainer").show()
-	hide()
+	get_parent().get_node("AnimationPlayer").play("on_open")
--- scripts/PauseMenu.gd
+++ scripts/PauseMenu.gd
@@ -3,6 +3,8 @@ extends CanvasLayer
 @onready var options_menu = get_node("OptionsMenu")
 @onready var pause_container = get_node("MainContainer")
 
+var is_option_pressed = false
+
 func _ready():
 	get_tree().paused = true
 
@@ -12,13 +14,18 @@ func _on_continue_pressed():
 	
 
 func _on_option_pressed():
+	is_option_pressed = true
+	$AnimationPlayer.play("on_close")
 	options_menu.show()
-	pause_container.hide()
+	options_menu.on_enable()
 
 func _on_exit_pressed():
 	$AnimationPlayer.play("on_close")
 	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
 
 func on_close_panel():
+	if is_option_pressed:
+		return
+	
 	get_tree().paused = false
 	queue_free()
