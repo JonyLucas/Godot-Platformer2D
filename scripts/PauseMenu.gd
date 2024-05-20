027744ef4ce3d6e2ccb4659543040137a0a1df9c
João Lucas <jony.lucas.br13@gmail.com>
2024-05-20 11:47:55 -0300

    Pause Menu Animation

--- scripts/PauseMenu.gd
+++ scripts/PauseMenu.gd
@@ -4,12 +4,17 @@ func _ready():
 	get_tree().paused = true
 
 func _on_continue_pressed():
+	$AnimationPlayer.play("on_close")
 	get_tree().paused = false
-	queue_free()
+	
 
 func _on_option_pressed():
 	pass # Replace with function body.
 
 func _on_exit_pressed():
-	get_tree().paused = false
+	$AnimationPlayer.play("on_close")
 	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
+
+func on_close_panel():
+	get_tree().paused = false
+	queue_free()
