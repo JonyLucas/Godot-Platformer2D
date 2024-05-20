1c202e098844e112e78489c5c2b61e3f13480cb3
João Lucas <jony.lucas.br13@gmail.com>
2024-05-20 00:17:21 -0300

    Initial Main Menu

---
+++ scripts/MainMenu.gd
@@ -0,0 +1,10 @@
+extends CanvasLayer
+
+func _on_play_button_pressed():
+	get_tree().change_scene_to_file("res://scenes/main.tscn")
+
+func _on_option_button_pressed():
+	pass # Replace with function body.
+
+func _on_exit_button_pressed():
+	get_tree().quit()
