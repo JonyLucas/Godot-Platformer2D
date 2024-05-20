db12632df2090d4a0031dd90721c4cab572f15af
João Lucas <jony.lucas.br13@gmail.com>
2024-05-20 18:48:20 -0300

    Tutorial Signs

---
+++ scripts/TutorialSign.gd
@@ -0,0 +1,26 @@
+extends Node2D
+
+@export_multiline var tutorial_text: String
+@export var label_text: Label
+
+var can_interact = false
+var panel_container
+
+func _ready():
+	panel_container = get_node("PanelContainer")
+	label_text.text = tutorial_text
+	panel_container.visible = false
+
+func _unhandled_input(event):
+	if event.is_action_pressed("interact") and can_interact:
+		panel_container.visible = true
+
+func _on_body_entered(_body:Node2D):
+	$Sprite2D.frame = 1
+	can_interact = true
+
+
+func _on_body_exited(_body:Node2D):
+	$Sprite2D.frame = 0
+	can_interact = false
+	panel_container.visible = false
